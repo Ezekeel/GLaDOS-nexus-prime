6fa25e1f7801ff6c10259fbc56593d4d59be8a9c
Ezekeel <notezekeel@googlemail.com>
2012-02-19 00:06:59 +0100

    Added kernel config option 'OMAP_OC_FREQSLOT'.

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -344,7 +344,11 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 	cpufreq_frequency_table_get_attr(freq_table, policy->cpu);
 
 	policy->min = policy->cpuinfo.min_freq;
+#ifdef CONFIG_OMAP_OC_FREQSLOT
+	policy->max = 1200000;
+#else
 	policy->max = policy->cpuinfo.max_freq;
+#endif
 	policy->cur = omap_getspeed(policy->cpu);
 
 	for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++)
