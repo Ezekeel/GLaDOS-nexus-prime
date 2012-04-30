56d625cb572d64a0b465bf0c3d00e5469003bc74
Ezekeel <notezekeel@googlemail.com>
2012-05-01 00:22:42 +0200

    Added kernel config option 'BCMDHD_WIFI_PM'.

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -523,6 +523,12 @@ static void dhd_set_packet_filter(int value, dhd_pub_t *dhd)
 }
 
 #if defined(CONFIG_HAS_EARLYSUSPEND)
+#ifdef CONFIG_BCMDHD_WIFI_PM
+static int wifi_pm = 0;
+
+module_param(wifi_pm, int, 0755);
+#endif
+
 static int dhd_set_suspend(int value, dhd_pub_t *dhd)
 {
 	int power_mode = PM_MAX;
@@ -533,6 +539,11 @@ static int dhd_set_suspend(int value, dhd_pub_t *dhd)
 
 	DHD_TRACE(("%s: enter, value = %d in_suspend=%d\n",
 		__FUNCTION__, value, dhd->in_suspend));
+
+#ifdef CONFIG_BCMDHD_WIFI_PM
+	if (wifi_pm == 1)
+	    power_mode = PM_FAST;
+#endif
 
 	if (dhd && dhd->up) {
 		if (value && dhd->in_suspend) {
