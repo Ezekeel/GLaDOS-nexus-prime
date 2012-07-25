2e536ea162b053a112d4d887e8f1b96c64a6c8dd
Ezekeel <notezekeel@googlemail.com>
2012-07-25 23:53:50 +0200

    Added kernel config options 'OMAP_SMARTREFLEX_CUSTOM_SENSOR', 'CONFIG_OMAP_OCFREQ_1400', 'CONFIG_OMAP_OCFREQ_1600', 'CONFIG_OMAP_OCFREQ_1800' and 'CONFIG_OMAP_OCFREQ_2000'.

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -344,7 +344,11 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 	cpufreq_frequency_table_get_attr(freq_table, policy->cpu);
 
 	policy->min = policy->cpuinfo.min_freq;
+#if defined(CONFIG_OMAP_OCFREQ_1400) || defined(CONFIG_OMAP_OCFREQ_1600) || defined(CONFIG_OMAP_OCFREQ_1800) || defined(CONFIG_OMAP_OCFREQ_2000)
+	policy->max = 1200000;
+#else
 	policy->max = policy->cpuinfo.max_freq;
+#endif
 	policy->cur = omap_getspeed(policy->cpu);
 
 	for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++)
